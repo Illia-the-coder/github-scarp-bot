9c8f34375b4f6ff9ad5fa2b8e3f2d58bebe9e0a1
illia Liudohovskyi <101904816+Illia-the-coder@users.noreply.github.com>
2024-08-03 21:12:44 +0200

    _

--- github_scraper/spiders/github_scraper.py
+++ github_scraper/spiders/github_scraper.py
@@ -1,17 +1,22 @@
 import scrapy
+import requests
 
 class GithubSpider(scrapy.Spider):
     name = 'github_scraper'
     allowed_domains = ['github.com']
 
     def start_requests(self):
-        with open('github_links.txt', 'r') as f:
-            urls = f.readlines()
+        file_url = 'https://raw.githubusercontent.com/Illia-the-coder/github-scarp-bot/main/github_scraper/github_links.txt'
+        try:
+            response = requests.get(file_url)
+            response.raise_for_status()
+            urls = response.text.splitlines()
             for url in urls:
                 yield scrapy.Request(url=url.strip(), callback=self.parse)
+        except requests.RequestException as e:
+            self.logger.error(f"Error fetching URLs: {e}")
 
     def parse(self, response):
-        
         yield {
             'username': response.xpath('//span[@class="p-name vcard-fullname d-block overflow-hidden"]/text()').get(default='').strip(),
             'nickname': response.xpath('//span[@class="p-nickname vcard-username d-block"]/text()').get(default='').strip(),
@@ -22,7 +27,7 @@ class GithubSpider(scrapy.Spider):
             'email': response.xpath('//a[contains(@href, "mailto:")]/@href').get(default='').replace('mailto:', ''),
             'bio': response.xpath('//div[@class="p-note user-profile-bio mb-3 js-user-profile-bio f4"]/div/text()').get(default='').strip(),
             'location': response.xpath('//li[@itemprop="homeLocation"]/span/text()').get(default='').strip(),
-            'public_repos': response.xpath('//span[@class="Counter"]/text()').extract()[0].strip() if response.xpath('//span[@class="Counter"]/text()').extract() else '0',
+            'public_repos': response.xpath('(//span[@class="Counter"])[1]/text()').get(default='0').strip(),
             'stars': response.xpath('//a[contains(@href, "?tab=stars")]/span[@class="Counter"]/text()').get(default='0').strip(),
             'organizations': len(response.xpath('//a[@data-hovercard-type="organization"]')),
             'followers': response.xpath('//a[contains(@href, "followers")]/span[@class="Counter"]/text()').get(default='0').strip(),
