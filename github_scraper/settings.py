c01c9e8cf04deb7a58550fe0d766b38a24b649c9
illia Liudohovskyi <101904816+Illia-the-coder@users.noreply.github.com>
2024-08-05 00:10:30 +0200

    _

--- github_scraper/settings.py
+++ github_scraper/settings.py
@@ -15,48 +15,20 @@ DEFAULT_REQUEST_HEADERS = {
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = False
 
 # Configure a delay for requests for the same website
-DOWNLOAD_DELAY = 10  # 2 seconds delay
+DOWNLOAD_DELAY = 5  # 2 seconds delay
 
-
-# Disable cookies (enabled by default)
-COOKIES_ENABLED = False
-
-# Enable and configure the AutoThrottle extension (disabled by default)
+# Enable and configure the AutoThrottle extension
 AUTOTHROTTLE_ENABLED = True
 AUTOTHROTTLE_START_DELAY = 1
 AUTOTHROTTLE_MAX_DELAY = 10
-AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0
-AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 
-# Enable and configure HTTP caching (disabled by default)
+# Enable and configure HTTP caching
 HTTPCACHE_ENABLED = True
 HTTPCACHE_EXPIRATION_SECS = 0
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
 HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
-
-# Retry middleware settings
-RETRY_ENABLED = True
-RETRY_TIMES = 10  # Adjust based on tolerance for rate limiting
-RETRY_HTTP_CODES = [429, 500, 502, 503, 504, 522, 524, 408, 403]
-
-# Download timeout
-DOWNLOAD_TIMEOUT = 30
-
-# Rotating user agents middleware
-# DOWNLOADER_MIDDLEWARES = {
-#     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-#     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
-#     'scrapy.downloadermiddlewares.retry.RetryMiddleware': 550,
-# }
-
-# Configure item pipelines (if needed)
-# ITEM_PIPELINES = {
-#    'github_scraper.pipelines.SomePipeline': 300,
-# }
-
-# Logging configuration
-# LOG_LEVEL = 'INFO'
--- github_scraper/spiders/github_scraper.py
+++ github_scraper/spiders/github_scraper.py
@@ -17,19 +17,20 @@ class GithubSpider(scrapy.Spider):
             self.logger.error(f"Error fetching URLs: {e}")
 
     def parse(self, response):
+        nickname = response.xpath('//span[@class="p-nickname vcard-username d-block"]/text()').get(default='').strip()
         yield {
             'username': response.xpath('//span[@class="p-name vcard-fullname d-block overflow-hidden"]/text()').get(default='').strip(),
-            'nickname': response.xpath('//span[@class="p-nickname vcard-username d-block"]/text()').get(default='').strip(),
+            'nickname': nickname,
             'twitter': response.xpath('//a[contains(@href, "twitter.com")]/@href').get(default=''),
             'instagram': response.xpath('//a[contains(@href, "instagram.com")]/@href').get(default=''),
             'linkedin': response.xpath('//a[contains(@href, "linkedin.com")]/@href').get(default=''),
-            'website': response.xpath('//a[@class="u-url"]/text()').get(default='').strip(),
+            'website': response.xpath('//li[@itemprop="url"]/a/text()').get(default='').strip(),
             'email': response.xpath('//a[contains(@href, "mailto:")]/@href').get(default='').replace('mailto:', ''),
             'bio': response.xpath('//div[@class="p-note user-profile-bio mb-3 js-user-profile-bio f4"]/div/text()').get(default='').strip(),
             'location': response.xpath('//li[@itemprop="homeLocation"]/span/text()').get(default='').strip(),
             'public_repos': int(response.xpath('(//span[@class="Counter"])[1]/text()').get(default='0').strip()),
             'stars': response.xpath('//a[contains(@href, "?tab=stars")]/span[@class="Counter"]/text()').get(default='0').strip(),
             'organizations': len(response.xpath('//a[@data-hovercard-type="organization"]')),
-            'followers': int(response.xpath('//a[contains(@href, "followers")]/svg/span/text()').get(default='0').strip()),
-            'following': int(response.xpath('//a[contains(@href, "following")]/svg/span[@class="Counter"]/text()').get(default='0').strip()),
+            'followers': int(response.xpath(f'//a[contains(@href, "?tab=followers")]/span/text()').get(default='0').replace(',', '')),
+            'following': int(response.xpath(f'//a[contains(@href, "?tab=following")]/span/text()').get(default='0').replace(',', '')),
         }
